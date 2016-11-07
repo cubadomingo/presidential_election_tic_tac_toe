a28bbe7afa8bcf4771c88c95261945ceaa0fda45
Devin Osorio <me@devinosor.io>
2016-11-07 14:37:26 -0800

    adds game view

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,21 @@
 class Game < ApplicationRecord
-  @game = [
-    ["","",""],
-    ["","",""],
-    ["","",""]
-  ]
+  def initialize
+    @game = [
+      ["X","O","X"],
+      ["X","O","X"],
+      ["X","X","X"]
+    ]
+  end
+
+  def row1
+    @game[0]
+  end
+
+  def row2
+    @game[1]
+  end
+
+  def row3
+    @game[2]
+  end
 end
